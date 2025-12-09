af538f0a20534baeaba1bb280f86977ee1627ff7
LeoCeratti <leonardo.ceratti@gmail.com>
2025-12-09 14:44:57 -0300

    Add files via upload

--- rickandmortyapi/rickandmortyapi/src/components/CharacterCard.jsx
+++ rickandmortyapi/rickandmortyapi/src/components/CharacterCard.jsx
@@ -1,12 +1,26 @@
+import "../styles/CharacterCard.css";
+
 export default function CharacterCard({ character }) {
+  function onError(e) {
+    e.currentTarget.src = "https://via.placeholder.com/300x300?text=No+Image";
+  }
+
   return (
     <div className="card">
-      <img src={character.image} alt={character.name} />
+      <img
+        src={character.image}
+        alt={character.name}
+        loading="lazy"
+        onError={onError}
+      />
 
       <h3>{character.name}</h3>
 
-      <p><b>Gênero:</b> {character.gender}</p>
-      <p><b>Origem:</b> {character.origin.name}</p>
+      <p><strong>Status:</strong> {character.status}</p>
+      <p><strong>Espécie:</strong> {character.species}</p>
+
+      <p><strong>Gênero:</strong> {character.gender}</p>
+      <p><strong>Origem:</strong> {character.origin?.name}</p>
     </div>
   );
 }
